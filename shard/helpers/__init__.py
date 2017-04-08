c35cf0b6a41aa155cff1b58729f4e9a3a4abcfab
Michael Roberts <marobs@umich.edu>
2017-04-08 14:38:02 -0400

    Removed shard subdir

--- shard/helpers/__init__.py
+++
@@ -1,3 +0,0 @@
-from shardData import ShardData
-from clientRequest import ClientRequest
-from hashHelper import hashKey
--- shard/helpers/hashHelper.py
+++
@@ -1,4 +0,0 @@
-from hashlib import md5
-
-def hashKey(key):
-    return md5.new(key).digest()
