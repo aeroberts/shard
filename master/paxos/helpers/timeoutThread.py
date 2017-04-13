5e101309c95e0b9916c32651f15919938b5eb211
Alex Roberts <aerobs@0587364691.wireless.umich.net>
2017-04-12 20:46:58 -0400

    Updated timeout functions to first attempt to send to master, then after timeout broadcast to cluster (both functions)

--- master/paxos/helpers/timeoutThread.py
+++ master/paxos/helpers/timeoutThread.py
@@ -1,19 +1,20 @@
-import threading
 import time
-import socket
-import messages
-import messageTypes
+import shardMessages
 
-#def broadcastSendKeyRequest(sock, oldShardAddrList, osMRV, nsMRV, lowerKeyBound, upperKeyBound, addrString):
-#def broadcastSendKeyResponse(sock, nsAddrs, osView, filteredKVStore):
+# def broadcastSendKeyRequest(sock, oldShardAddrList, osMRV, nsMRV, lowerKeyBound, upperKeyBound, addrString):
+# def broadcastSendKeyResponse(sock, nsAddrs, osView, filteredKVStore):
 
 # Client must call sock.close() before calling thread.kill()
 # Must be called like oldShardAddrList[:], int(o/nsMRV), int(upper/lowerKB), str(addrString) so they are copies not references
-def broadcastSendKeyRequestWithTimeout(sock, oldShardAddrList, osMRV, nsMRV, lowerKeyBound, upperKeyBound, addrString):
+def sendSendKeyRequestWithTimeout(sock, msn, oldShardAddrList, osMRV, nsMRV,
+                                       lowerKeyBound, upperKeyBound, addrString):
+    shardMessages.sendSendKeysRequest(sock, msn, oldShardAddrList, osMRV, nsMRV,
+                                          lowerKeyBound, upperKeyBound, addrString)
     while True:
+        time.sleep(1)
         try:
-            messages.broadcastSendKeyRequest(sock, oldShardAddrList, osMRV, nsMRV, lowerKeyBound, upperKeyBound, addrString)
-            time.sleep(1)
+            shardMessages.broadcastSendKeyRequest(sock, msn, oldShardAddrList, osMRV, nsMRV,
+                                             lowerKeyBound, upperKeyBound, addrString)
         except:
             print "EXCEPT IN THREAD2"
             return
@@ -21,11 +22,12 @@ def broadcastSendKeyRequestWithTimeout(sock, oldShardAddrList, osMRV, nsMRV, low
 
 # Client must call sock.close() before calling thread.kill()
 # Must be called like nsAddrs[:], int(osView), filteredKVStore[:] so they are copies not references
-def broadcastSendKeyResponseWithTimeout(sock, nsAddrs, osView, filteredKVStore):
+def sendSendKeyResponseWithTimeout(sock, msn, nsAddrs, osView, nsView, filteredKVStore):
+    shardMessages.sendSendKeysResponse(sock, msn, nsAddrs, osView, nsView, filteredKVStore)
     while True:
+        time.sleep(1)
         try:
-            messages.broadcastSendKeyResponse(sock, nsAddrs, osView, filteredKVStore)
-            time.sleep(1)
+            shardMessages.broadcastSendKeyResponse(sock, msn, nsAddrs, osView, nsView, filteredKVStore)
         except:
             print "EXCEPT IN THREAD"
             return
