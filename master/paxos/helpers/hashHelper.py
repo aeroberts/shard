a113c9b451763934906aada7429baced9b23e855
Michael Roberts <marobs@umich.edu>
2017-04-14 00:15:06 -0400

    Added commented-out debugging print

--- master/paxos/helpers/hashHelper.py
+++ master/paxos/helpers/hashHelper.py
@@ -1,4 +1,7 @@
 from hashlib import md5
 
 def hashKey(key):
+    #digest = md5.new(key).digest()
+    #print "Digest type: " + type(digest) + " - value: " + str(digest) + " - as long: " + str(long(digest))
     return md5.new(key).digest()
+
